f4bf2fd96512aaa1679b5b267bac26dfae138215
maksat sarsenbaev <88890025m@gmail.com>
2024-02-10 22:02:28 +0600

    added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(process.env.PORT || 3000);
   console.log(`server ok PORT ${process.env.PORT || 3000}`);
 }
